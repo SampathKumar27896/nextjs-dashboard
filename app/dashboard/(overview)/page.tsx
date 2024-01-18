b415650de913ab5c46d514c5bb31d84ddd98d475
Sampathkumar27896 <sampathkumar27896@hotmail.com>
2024-01-18 17:06:02 +0530

    Added logic for dynamic fetching of data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,7 @@
 import  CardWrapper  from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { LusitanaFont } from '@/app/ui/font';
+import { InterFont } from '@/app/ui/font';
 import {  fetchCardData  } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
@@ -10,7 +10,7 @@ export default async function Page() {
     const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
   return (
     <main>
-      <h1 className={`${LusitanaFont.className} mb-4 text-xl md:text-2xl`}>
+      <h1 className={`${InterFont.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,14 +14,14 @@ import { unstable_noStore as noStore} from 'next/cache';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    noStore();
+    
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
@@ -34,8 +34,9 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
   try {
-    noStore();
+    
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -55,8 +56,9 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
   try {
-    noStore();
+   
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
@@ -97,9 +99,9 @@ export async function fetchFilteredInvoices(
   currentPage: number,
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-
+  noStore();
   try {
-    noStore();
+    
     const invoices = await sql<InvoicesTable>`
       SELECT
         invoices.id,
@@ -129,8 +131,9 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore();
   try {
-    noStore();
+    
     const count = await sql`SELECT COUNT(*)
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
@@ -151,8 +154,9 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
   try {
-    noStore();
+   
     const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
@@ -177,8 +181,9 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore();
   try {
-    noStore();
+   
     const data = await sql<CustomerField>`
       SELECT
         id,
@@ -196,8 +201,9 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore();
   try {
-    noStore();
+    
     const data = await sql<CustomersTableType>`
 		SELECT
 		  customers.id,
@@ -230,8 +236,9 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
+  noStore();
   try {
-    noStore();
+   
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
   } catch (error) {
